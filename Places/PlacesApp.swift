556a4f86b6de1a2d524b69cea95a8e1b2782af86
Nate Payne Potter <nate.potter@nl.abnamro.com>
2024-06-21 15:56:45 +0200

    Initial Commit

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Nate Payne Potter on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
